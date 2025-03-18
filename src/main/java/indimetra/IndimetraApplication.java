6cc5b16ffaf9889f20f331c6f16cfc49cb6b8acf
diegoarnanz-maker <diegoarnanzlozano96@gmail.com>
2025-03-18 09:08:34 +0100

    Primer commit: Proyecto de Spring Boot iniciado

---
+++ src/main/java/indimetra/IndimetraApplication.java
@@ -0,0 +1,13 @@
+package indimetra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndimetraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndimetraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/indimetra/IndimetraApplicationTests.java
@@ -0,0 +1,13 @@
+package indimetra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndimetraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
